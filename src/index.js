d0677bde78ef833d2b759bc8bf9c7cde083e9d9e
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2018-04-11 23:23:51 +0200

    silently fail on missing/wrong keyName

--- src/index.js
+++ src/index.js
@@ -5,23 +5,27 @@ import { data } from "./data";
 // this produces a flat html structure
 // replacing the Fragments with ul and li respectively
 // would produce a "normal" nested html list structure
-const NestedList = ({ data, keyName, level = 0, render }) => (
-  <Fragment>
-    {data[keyName].map(item => (
-      <Fragment key={item.id}>
-        {render(item, level)}
-        {item[keyName] && (
-          <NestedList
-            data={item}
-            keyName={keyName}
-            level={level + 1}
-            render={render}
-          />
-        )}
-      </Fragment>
-    ))}
-  </Fragment>
-);
+const NestedList = ({ data, keyName, level = 0, render }) => {
+  if (!data[keyName]) return null;
+
+  return (
+    <Fragment>
+      {data[keyName].map(item => (
+        <Fragment key={item.id}>
+          {render(item, level)}
+          {item[keyName] && (
+            <NestedList
+              data={item}
+              keyName={keyName}
+              level={level + 1}
+              render={render}
+            />
+          )}
+        </Fragment>
+      ))}
+    </Fragment>
+  );
+};
 
 // The rendering component gets a "level" prop that can be
 // used for any type of conditional rendering or styling
